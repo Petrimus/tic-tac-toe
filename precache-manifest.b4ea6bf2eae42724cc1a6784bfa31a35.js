f75118c2d64a1977a3287eb7a5dd205fc6af39e3
Petrimus <petrimus76@gmail.com>
2020-01-13 16:46:15 +0200

    Updates

---
+++ precache-manifest.b4ea6bf2eae42724cc1a6784bfa31a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bdce6acc0be46113965796ece232c4",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "9a4d24266ea38022c755",
+    "url": "/tic-tac-toe/static/css/main.298554d1.chunk.css"
+  },
+  {
+    "revision": "f0b56ed91f4afcd86c1f",
+    "url": "/tic-tac-toe/static/js/2.9377cd82.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tic-tac-toe/static/js/2.9377cd82.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a4d24266ea38022c755",
+    "url": "/tic-tac-toe/static/js/main.37a221ec.chunk.js"
+  },
+  {
+    "revision": "27862439030061b4d5e8",
+    "url": "/tic-tac-toe/static/js/runtime-main.bf9aa495.js"
+  }
+]);
